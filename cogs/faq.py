4ed4f165834b277c1292f9211f59788eaeb7a95f
InvisibleOS <76255686+InvisibleOS@users.noreply.github.com>
2021-10-29 18:37:49 +0530

    Update faq.py

--- cogs/faq.py
+++ cogs/faq.py
@@ -38,7 +38,7 @@ class Faq(commands.Cog):
         )
         embed.add_field(
             name="- Does disnake have slash commands?",
-            value="Yes, it is completely up to date with Discord. It has all interactions (such as buttons and select menus) and application commands (slash commands and context menus)",
+            value="Yes, it is completely up to date with Discord. It has all interactions (such as buttons and select menus) and application commands (slash commands and context menus).\n([Click here to learn how learn how to create a slash command using disnake](https://disnake.readthedocs.io/en/latest/ext/commands/slash_commands.html#slash-commands))",
             inline=False
         )
         embed.add_field(
